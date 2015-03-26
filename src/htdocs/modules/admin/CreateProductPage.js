26e074d5d2a7852e91624b7bee02c6c178234d9f
Jeremy Fee <jmfee@usgs.gov>
2015-03-26 16:01:20 -0600

    Update create product page to work with modal send product view

--- src/htdocs/modules/admin/CreateProductPage.js
+++ src/htdocs/modules/admin/CreateProductPage.js
@@ -10,19 +10,46 @@ var EventModulePage = require('base/EventModulePage'),
 var CreateProductPage = function (options) {
   options = Util.extend({}, options || {});
 
+  this._sendProductButton = null;
+  this._sendProductView = null;
+
   EventModulePage.call(this, options);
 };
 CreateProductPage.prototype = Object.create(EventModulePage.prototype);
 
 
 CreateProductPage.prototype._setContentMarkup = function () {
-  var el = this._content,
-      products = this._event.properties.products;
+  var el = this._content;
 
-  this.sendProductView = SendProductView({
-    el: el,
+  el.innerHTML = '<button class="sendProduct">Send Product</button>';
+  this._sendProductButton = el.querySelector('.sendProduct');
+
+  this._onSendProductClick = this._onSendProductClick.bind(this);
+  this._sendProductButton.addEventListener('click', this._onSendProductClick);
+};
+
+CreateProductPage.prototype._onSendProductClick = function () {
+  var products = this._event.properties.products;
+
+  this._sendProductView = SendProductView({
     product: Product(products.origin[0])
   });
+  this._sendProductView.on('cancel', this.onRemove, this);
+};
+
+CreateProductPage.prototype.onRemove = function () {
+  if (this._sendProductView !== null) {
+    this._sendProductView.destroy();
+    this._sendProductView = null;
+  }
+};
+
+
+CreateProductPage.prototype.destroy = function () {
+  this.onRemove();
+  this._sendProductButton.removeEventListener(this._onSendProductClick);
+  this._onSendProductClick = null;
+  this._sendProductButton = null;
 };
 
 
