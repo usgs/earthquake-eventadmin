59387b3425b9e471868ee5acd82a4b3b8112f38a
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-03-31 12:42:43 -0600

    switched from data to products and cleaned up destroy method

--- src/htdocs/js/EditLinkView.js
+++ src/htdocs/js/EditLinkView.js
@@ -4,82 +4,78 @@ var ModalView = require('mvc/ModalView');
     Util = require('util/Util'),
     View = require('mvc/View'),
 
-    ProductSender = require('ProductSender');
-
-
-var DATA_DEFAULTS = {
-  id: null,
-  text: null,
-  type: null,
-  url: null
-};
+    SendProductView = require('../modules/admin/SendProductView.js');
 
 var EditLinkView = function (options) {
   var _this,
       _initialize,
 
-      _cancel,
-      _data,
-      _form,
       _modal,
-      _sender,
+      _product,
+      _sendProductView,
       _text,
       _url,
 
-      _onSubmit,
-      _onCancel;
+      _onCancel,
+      _onDone,
+      _onSubmit;
 
   _this = View(options);
 
   _initialize = function (options) {
-    _data = Util.extend({}, DATA_DEFAULTS, options.data);
-    _sender = options.sender || ProductSender();
+    _product = options.product;
+    _sendProductView = SendProductView({
+      product: _product
+    });
+
+    _sendProductView.on('done', _onDone);
 
     content =
-      '<div>' +
-        '<label for="linkText">Link Text</label>' +
-        '<small>Text to display on web page</small>' +
-        '<input id="linkText" name="linkText" type="text"/>' +
-        '<br/>' +
-        '<label for="linkURL">Link URL</label>' +
-        '<small>Link to display on web page</small>' +
-        '<input id="linkURL" name="linkURL" type="text"/>' +
-      '</div>';
+        '<div>' +
+          '<label for="linkText">Link Text</label>' +
+          '<small>Text to display on web page</small>' +
+          '<input id="linkText" name="linkText" type="text"/>' +
+          '<br/>' +
+          '<label for="linkURL">Link URL</label>' +
+          '<small>Link to display on web page</small>' +
+          '<input id="linkURL" name="linkURL" type="text"/>' +
+        '</div>';
 
-      _modal = ModalView(content, {
-        title: 'Add/Edit Link',
-        closable: false,
-        buttons: [
-          {
-            classes: ['save', 'green' ],
-            text:'Send',
-            callback: _onSubmit
-          },
-          {
-            classes: ['cancel'],
-            text: 'Cancel',
-            callback: _onCancel
-          }
-        ]
-      });
+    _modal = ModalView(content, {
+      title: 'Add/Edit Link',
+      closable: false,
+      buttons: [
+        {
+          classes: ['save', 'green' ],
+          text:'Send',
+          callback: _onSubmit
+        },
+        {
+          classes: ['cancel'],
+          text: 'Cancel',
+          callback: _onCancel
+        }
+      ]
+    });
 
     _text = _this.el.querySelector('#linkText');
     _url = _this.el.querySelector('#linkURL');
-    _cancel = _this.el.querySelector('.cancel');
-
-    _form.addEventListener('submit', _onSubmit);
-    _cancel.addEventListener('click', _onCancel);
 
     options = null;
   };
 
   _onSubmit = function () {
-    _data.text = _text;
-    _data.url = _url;
-    _modal.el.querySelector('.save').disabled = true;
+    var props;
 
-    //_this.trigger('save', _data);
-    _sender.sendProduct('save', _data);
+    props = Util.extend({}, _product.get('properties'));
+    props.text = _text.value;
+    props.url = _url.value;
+
+    _product.set({
+      propeerties: props
+    });
+
+    _sendProductView.show();
   };
 
   _onCancel = function () {
@@ -87,17 +83,47 @@ var EditLinkView = function (options) {
     _this.trigger('cancel');
   };
 
+  _onDone = function () {
+    _this.trigger('done');
+    _this.destroy();
+  };
+
+  _this.render = function () {
+    var props,
+        text,
+        url;
+
+    props = _product.get('properties');
+    text = props.text || '';
+    url = props.url || '';
+
+    _text.value = text;
+    _url.value = url;
+  };
+
+  _this.hide = function () {
+    _dialog.hide();
+  };
+
+  _this.show = function () {
+    _this.render();
+    _dialog.show();
+  };
+
   _this.destroy = Util.compose(function () {
     _modal.hide();
     _modal.destroy();
-    _cancel = null;
-    _data = null;
-    _form = null;
-    _modal = null;
-    _sender = null;
+    if (_modal !== null) {
+      _modal = Null;
+    }
+    if (_sender !== null) {
+      _sender = null;
+    }
     _text = null;
     _url = null;
-    _onSubmit = null;
+    if (_onSubmit !== null) {
+      _onSubmit = null;
+    }
     _onCancel = null;
   }, _this.destroy);
 
