7cc0c277e08677ac1efb6ad4669d94c9689939d4
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-03-31 09:21:49 -0600

    Changed code to be more inline with SendProductView and the way it is using modal view

--- src/htdocs/js/EditLinkView.js
+++ src/htdocs/js/EditLinkView.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var View = require('mvc/View'),
-    Util = require('util/Util');
+var ModalView = require('mvc/ModalView');
+    Util = require('util/Util'),
+    View = require('mvc/View'),
+
+    ProductSender = require('ProductSender');
 
 
 var DATA_DEFAULTS = {
@@ -18,6 +21,8 @@ var EditLinkView = function (options) {
       _cancel,
       _data,
       _form,
+      _modal,
+      _sender,
       _text,
       _url,
 
@@ -26,45 +31,75 @@ var EditLinkView = function (options) {
 
   _this = View(options);
 
-
   _initialize = function (options) {
     _data = Util.extend({}, DATA_DEFAULTS, options.data);
+    _sender = options.sender || ProductSender();
 
-    _this.el.innerHTML =
+    content =
       '<div>' +
-        '<form class="linkForm">' +
-          '<label for="linkText">Link Text</label>' +
-          '<small>Text to display on web page</small>' +
-          '<input id="linkText" name="linkText" type="text"/>' +
-          '<br/>' +
-          '<label for="linkURL">Link URL</label>' +
-          '<small>Text to display on web page</small>' +
-          '<input id="linkURL" name="linkURL" type="text"/>' +
-          '<br/>' +
-          '<button class="save">Save Link</button>' +
-          '<button class="cancel">Cancel</button>' +
-        '</form>' +
+        '<label for="linkText">Link Text</label>' +
+        '<small>Text to display on web page</small>' +
+        '<input id="linkText" name="linkText" type="text"/>' +
+        '<br/>' +
+        '<label for="linkURL">Link URL</label>' +
+        '<small>Link to display on web page</small>' +
+        '<input id="linkURL" name="linkURL" type="text"/>' +
       '</div>';
 
-    _form = _this.el.querySelector('.linkForm');
+      _modal = ModalView(content, {
+        title: 'Add/Edit Link',
+        closable: false,
+        buttons: [
+          {
+            classes: ['save', 'green' ],
+            text:'Send',
+            callback: _onSubmit
+          },
+          {
+            classes: ['cancel'],
+            text: 'Cancel',
+            callback: _onCancel
+          }
+        ]
+      });
+
     _text = _this.el.querySelector('#linkText');
     _url = _this.el.querySelector('#linkURL');
     _cancel = _this.el.querySelector('.cancel');
 
     _form.addEventListener('submit', _onSubmit);
     _cancel.addEventListener('click', _onCancel);
+
+    options = null;
   };
 
-  _onSubmit = function (_data) {
+  _onSubmit = function () {
     _data.text = _text;
     _data.url = _url;
+    _modal.el.querySelector('.save').disabled = true;
 
-    _this.trigger('save', _data);
+    //_this.trigger('save', _data);
+    _sender.sendProduct('save', _data);
   };
 
   _onCancel = function () {
+    _dialog.hide();
     _this.trigger('cancel');
   };
+
+  _this.destroy = Util.compose(function () {
+    _modal.hide();
+    _modal.destroy();
+    _cancel = null;
+    _data = null;
+    _form = null;
+    _modal = null;
+    _sender = null;
+    _text = null;
+    _url = null;
+    _onSubmit = null;
+    _onCancel = null;
+  }, _this.destroy);
 
   _initialize();
   return _this;
