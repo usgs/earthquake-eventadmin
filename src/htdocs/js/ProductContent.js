4c680961b435c262312f99d24e90e1b93f135956
Eric Martinez <emartinez@usgs.gov>
2015-05-05 14:14:54 -0600

    Unset bytes/url when the other is set. Added validation.

--- src/htdocs/js/ProductContent.js
+++ src/htdocs/js/ProductContent.js
@@ -4,6 +4,20 @@ var Model = require('mvc/Model'),
 
     Util = require('util/Util');
 
+var _attr_is_valid = function (attrs, attr) {
+  var value;
+
+  if (attrs.hasOwnProperty(attr)) {
+    value = attrs[attr];
+
+    if (value !== null && typeof value !== 'undefined') {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 var ProductContent = function (options) {
   var _this,
       _initialize;
@@ -13,13 +27,11 @@ var ProductContent = function (options) {
     contentType: 'text/plain',
     id: null,
     lastModified: (new Date()).getTime(),
-    length: 0,
-    path: null
+    length: 0
   }, options));
 
-  _initialize = function (options) {
+  _initialize = function (/*options*/) {
     var bytes = _this.get('bytes'),
-        path = _this.get('path'),
         url = _this.get('url');
 
     if (bytes === null && url === null) {
@@ -31,12 +43,44 @@ var ProductContent = function (options) {
       throw new Error('Invalid product contents. Actual length and claimed ' +
           'length differ.');
     }
+  };
 
-    if (path === null) {
-      throw new Error('Invalid product contents. A path must be specified.');
+
+  _this.set = Util.compose(function (attributes) {
+    var hasBytes,
+        hasUrl;
+
+    hasBytes = attributes.hasOwnProperty('bytes');
+    hasUrl = attributes.hasOwnProperty('url');
+
+    // ProductContent has either bytes, or url, but not both. If one is set,
+    // clear the other...
+    if (hasUrl && attributes.url !== null) {
+      attributes.bytes = null;
+    } else if (hasBytes && attributes.bytes !== null) {
+      attributes.url = null;
     }
 
-    _this.set({id: options.id || path}, {silent: true});
+    return attributes;
+  }, _this.set);
+
+  _this.validate = function () {
+    var attrs,
+        error;
+
+    attrs = _this.get();
+
+    if (!_attr_is_valid(attrs, 'id')) {
+      error = new Error('Product content must have a valid path');
+    } else if (!_attr_is_valid(attrs, 'url') &&
+        !_attr_is_valid(attrs, 'bytes')) {
+      error = new Error('Product content must have either direct upload ' +
+          'content or file upload content.');
+    }
+
+    if (error) {
+      throw error;
+    }
   };
 
   _initialize(options);
