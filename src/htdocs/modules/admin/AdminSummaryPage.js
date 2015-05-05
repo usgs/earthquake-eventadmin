5e5844c649487ea1cb667e68f3ba12778d7ea32b
Jeremy Fee <jmfee@usgs.gov>
2015-05-05 14:54:07 -0600

    Add Invalidate button to admin summary page

--- src/htdocs/modules/admin/AdminSummaryPage.js
+++ src/htdocs/modules/admin/AdminSummaryPage.js
@@ -3,12 +3,16 @@
 var EventsAssociatedView = require('admin/EventsAssociatedView'),
     EventsNearbyView = require('admin/EventsNearbyView'),
     EventModulePage = require('base/EventModulePage'),
+    InvalidatorView = require('invalidator/InvalidatorView'),
+    ModalView = require('mvc/ModalView'),
     Util = require('util/Util');
 
 
 var AdminSummaryPage = function (options) {
 
   options = Util.extend({}, options || {});
+
+  this._actions = null;
 
   this._searchUrl = 'http://' + options.eventConfig.OFFSITE_HOST +
       options.eventConfig.SEARCH_STUB;
@@ -19,18 +23,75 @@ var AdminSummaryPage = function (options) {
 AdminSummaryPage.prototype = Object.create(EventModulePage.prototype);
 
 AdminSummaryPage.prototype._setContentMarkup = function () {
+  var button,
+      content = this._content;
 
-  EventsAssociatedView({
-    el: this._content,
+  content.innerHTML =
+      '<div class="actions">' +
+        '<button class="invalidate">Invalidate Cache</button>' +
+      '</div>' +
+      '<div class="events-associated"></div>' +
+      '<div class="events-nearby"></div>';
+
+  button = content.querySelector('.invalidate');
+  button._clickHandler = this._onInvalidateClick.bind(this);
+  button.addEventListener('click', button._clickHandler);
+  this._invalidateButton = button;
+
+  this._eventsAssociated = EventsAssociatedView({
+    el: content.querySelector('.events-associated'),
     eventDetails: this._event
   });
 
-  EventsNearbyView({
-    el: this._content,
+  this._eventsNearby = EventsNearbyView({
+    el: content.querySelector('.events-nearby'),
     eventDetails: this._event,
     searchUrl: this._searchUrl
   });
 
+};
+
+AdminSummaryPage.prototype._onInvalidateClick = function () {
+  var eventid = this._event.id,
+      paths,
+      view,
+      modal;
+
+  paths = [];
+  paths.push('/earthquakes/eventpage/' + eventid);
+  paths.push('/earthquakes/feed/v1.0/detail/' + eventid + '.geojson');
+  paths.push('/fdsnws/event/1/query?eventid=' + eventid + '&format=geojson');
+
+  view = InvalidatorView({
+    paths: paths
+  });
+
+  modal = ModalView(view.el, {
+    closable: true,
+    title: 'Invalidate Cache'
+  });
+  modal.on('hide', function () {
+    view.destroy();
+    modal.destroy();
+  });
+  modal.show();
+};
+
+AdminSummaryPage.prototype.destroy = function () {
+  var button;
+
+  button = this._invalidateButton;
+  button.removeEventListener('click', button._clickHandler);
+  button._clickHandler = null;
+  this._invalidateButton = null;
+
+  this._eventsAssociated.destroy();
+  this._eventsAssociated = null;
+
+  this._eventsNearby.destroy();
+  this._eventsNearby = null;
+
+  EventModulePage.prototype.destroy.call(this);
 };
 
 
