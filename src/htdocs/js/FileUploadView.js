40e6eba66efcec2b4a70c9c3be19437129963f2c
Eric Martinez <emartinez@usgs.gov>
2015-05-07 07:47:51 -0600

    Refactored to let upload view use custom drop targets and action buttons.

--- src/htdocs/js/FileUploadView.js
+++ src/htdocs/js/FileUploadView.js
@@ -38,12 +38,14 @@ var FileUploadView = function (options) {
   var _this,
       _initialize,
       // variables
+      _buttonEl,
       _dropEl,
       _inputEl,
       _uploadEl,
       _url,
       _hideOnSuccess,
       // methods
+      _onButtonClick,
       _onFile,
       _onDragLeave,
       _onDragOver,
@@ -52,41 +54,88 @@ var FileUploadView = function (options) {
 
   _this = View(options);
 
-  _initialize = function () {
-    var el = _this.el;
-
+  _initialize = function (options) {
     options = Util.extend({}, DEFAULTS, options);
 
     _url = options.url;
     _hideOnSuccess = options.hideOnSuccess;
 
-    options = null;
-
-    el.innerHTML =
-        '<div class="fileUploadView">' +
-          '<div class="chooseUpload">' +
-            '<form class="fileForm vertical">' +
-              '<label for="fileUploadInput">Choose file(s) to upload</label>' +
-              '<input type="file" id="fileUploadInput" class="fileInput"/>' +
-            '</form>' +
-            '<div class="fileDrop">' +
-              '<p>Drop file(s) here</p>' +
-            '</div>' +
-          '</div>' +
-          '<div class="uploadStatus"></div>' +
-        '</div>';
-
-    _dropEl = el.querySelector('.fileDrop');
+    _dropEl = _this.el;
     _dropEl.addEventListener('dragleave', _onDragLeave);
     _dropEl.addEventListener('dragover', _onDragOver);
     _dropEl.addEventListener('drop', _onDrop);
 
-    _inputEl = el.querySelector('.fileInput');
-    _inputEl.addEventListener('change', _onDrop);
+    _buttonEl = options.button || document.createElement('button');
+    _buttonEl.addEventListener('click', _onButtonClick);
 
-    _uploadEl = el.querySelector('.uploadStatus');
+    _uploadEl = options.upload || document.createElement('div');
+
+    _inputEl = document.createElement('input');
+    _inputEl.setAttribute('type', 'file');
+    _inputEl.addEventListener('change', _onDrop);
   };
 
+
+  /**
+   * Called when the button is clicked. Serves as a simple proxy for the
+   * input button but is more manageable for visual display.
+   *
+   */
+  _onButtonClick = function () {
+    var evt;
+
+    if (_inputEl.click) {
+      _inputEl.click();
+    } else {
+      evt = document.createEvent('HTMLEvents');
+      evt.initEvent('click', true, true);
+      _inputEl.dispatchEvent(evt);
+    }
+  };
+
+  /**
+   * Called when drag leaves drop target, and by _onDrop.
+   *
+   * @param e {DOMEvent}
+   *        drag/drop event.
+   */
+  _onDragLeave = function (e) {
+    e.preventDefault();
+    _dropEl.classList.remove('drag-over');
+  };
+
+  /**
+   * Called when drag enters drop target.
+   *
+   * @param e {DOMEvent}
+   *        drag event.
+   */
+  _onDragOver = function (e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+    _dropEl.classList.add('drag-over');
+  };
+
+  /**
+   * Called when drop, or file input change, occurs.
+   *
+   * @param e {DOMEvent}
+   *        drop/change event
+   */
+  _onDrop = function (e) {
+    var files,
+        i;
+
+    _onDragLeave(e);
+
+    files = e.target.files || e.dataTransfer.files;
+    for (i = 0; i < files.length; i++) {
+      _onFile(files[i]);
+    }
+
+    // clear input in case that was source of event
+    _inputEl.value = '';
+  };
 
   /**
    * Called with each file either dropped on drop target,
@@ -129,48 +178,6 @@ var FileUploadView = function (options) {
     data = null;
   };
 
-  /**
-   * Called when drag leaves drop target, and by _onDrop.
-   *
-   * @param e {DOMEvent}
-   *        drag/drop event.
-   */
-  _onDragLeave = function (e) {
-    e.preventDefault();
-    _dropEl.classList.remove('drag-over');
-  };
-
-  /**
-   * Called when drag enters drop target.
-   *
-   * @param e {DOMEvent}
-   *        drag event.
-   */
-  _onDragOver = function (e) {
-    e.preventDefault();
-    _dropEl.classList.add('drag-over');
-  };
-
-  /**
-   * Called when drop, or file input change, occurs.
-   *
-   * @param e {DOMEvent}
-   *        drop/change event
-   */
-  _onDrop = function (e) {
-    var files,
-        i;
-
-    _onDragLeave(e);
-
-    files = e.target.files || e.dataTransfer.files;
-    for (i = 0; i < files.length; i++) {
-      _onFile(files[i]);
-    }
-
-    // clear input in case that was source of event
-    _inputEl.value = '';
-  };
 
   /**
    * Destroy this view.
@@ -184,16 +191,28 @@ var FileUploadView = function (options) {
     _dropEl.removeEventListener('dragleave', _onDragLeave);
     _dropEl.removeEventListener('dragover', _onDragOver);
     _dropEl.removeEventListener('drop', _onDrop);
+
+    _buttonEl.removeEventListener('click', _onButtonClick);
+
     _inputEl.removeEventListener('change', _onDrop);
 
+    _buttonEl = null;
     _dropEl = null;
     _inputEl = null;
     _uploadEl = null;
+
+    _onButtonClick = null;
+    _onDragLeave = null;
+    _onDragOver = null;
+    _onDrop = null;
+    _onFile = null;
+
     _this = null;
   }, _this.destroy);
 
 
-  _initialize();
+  _initialize(options);
+  options = null;
   return _this;
 };
 
