d821e84ad0c4109afa0d535507875995f21f9ec8
Eric Martinez <emartinez@usgs.gov>
2015-05-07 07:56:21 -0600

    Updated tests for refactor.

--- test/spec/TextProductViewTest.js
+++ test/spec/TextProductViewTest.js
@@ -25,7 +25,7 @@ describe('TextProductView', function () {
             eventSourceCode: 'testEventSourceCode'
           },
           contents: [
-            {path: '', bytes: ''}
+            {id: '', bytes: ''}
           ]
         })
       });
@@ -49,7 +49,6 @@ describe('TextProductView', function () {
 
       expect(textProductView).to.respondTo('destroy');
       expect(textProductView).to.respondTo('hide');
-      expect(textProductView).to.respondTo('render');
       expect(textProductView).to.respondTo('show');
 
       textProductView.destroy();
@@ -69,57 +68,18 @@ describe('TextProductView', function () {
       });
 
       /* jshint -W030 */
-      expect(document.querySelector('.textproduct')).to.be.null;
+      expect(document.querySelector('.text-product-view')).to.be.null;
       /* jshint +W030 */
 
       textProductView.show();
       /* jshint -W030 */
-      expect(document.querySelector('.textproduct')).not.to.be.null;
+      expect(document.querySelector('.text-product-view')).not.to.be.null;
       /* jshint +W030 */
 
       textProductView.hide();
       /* jshint -W030 */
-      expect(document.querySelector('.textproduct')).to.be.null;
+      expect(document.querySelector('.text-product-view')).to.be.null;
       /* jshint +W030 */
-
-      textProductView.destroy();
-    });
-  });
-
-  describe('Render', function () {
-
-    it('Accurately reflects product values', function () {
-      var productText = 'This is some cool test text.';
-
-      var textProductView = TextProductView({
-        product: Product({
-          type: 'testType',
-          source: 'testSource',
-          code: 'testCode',
-          properties: {
-            eventsource: 'testEventSource',
-            eventsourcecode: 'testEventSourceCode'
-          },
-          contents: [
-            {
-              bytes: productText,
-              id: '',
-              lastModified: (new Date()).getTime(),
-              length: productText.length,
-              type: 'text/html'
-            }
-          ]
-        })
-      });
-
-      var el = textProductView.el,
-          textEl = el.querySelector('.textproduct-text');
-
-      expect(textEl.value).to.not.equal(productText);
-
-      textProductView.render();
-
-      expect(textEl.value).to.equal(productText);
 
       textProductView.destroy();
     });
