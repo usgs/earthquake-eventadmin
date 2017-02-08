fb7691a66d3b1f939be8deb014e1db5c31e9d6e8
emartinez-usgs <emartinez@usgs.gov>
2017-02-07 17:48:48 -0700

    Re-adding accordion since it gets used.

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -13,6 +13,7 @@ var browserify = {
       paths: [
         CWD + '/' + config.src + '/htdocs/js',
         CWD + '/' + config.src + '/htdocs/modules',
+        NODE_MODULES + '/hazdev-accordion/src',
         NODE_MODULES + '/hazdev-cache-invalidator/src/htdocs/js',
         NODE_MODULES + '/hazdev-webutils/src'
       ]
