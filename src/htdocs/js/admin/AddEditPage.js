d2f13af5e01f51a63517e726561b0f2bde508dc3
emartinez-usgs <emartinez@usgs.gov>
2017-02-09 07:34:09 -0700

    Added method comments.

--- src/htdocs/js/admin/AddEditPage.js
+++ src/htdocs/js/admin/AddEditPage.js
@@ -53,8 +53,11 @@ var AddEditPage = function (options) {
 
 
   /**
-   * Creates the scaffold for the this page along with sub-views for each
-   * specific task.
+   * Create the skeleton scaffold for this page. This page includes "pin" items
+   * for each action to be taken (adding/editing specific products/content).
+   * This function should create each pin item and bind event handlers to the
+   * corresponding buttons in order to open the dialog to accomplish a specific
+   * task and/or add/edit products/content.
    *
    */
   _createViewSkeleton = function () {
@@ -76,6 +79,24 @@ var AddEditPage = function (options) {
   };
 
 
+  /**
+   * Create markup for a new pin item to display.
+   *
+   * @param className {String}
+   *     The class name to add to the pin item container. This class should
+   *     subsequently be used to scope a selector to get the button element
+   *     in order to bind an event handler to it.
+   * @param title {String}
+   *     The pin title for this item. Appears at the top of the pin.
+   * @param content {String}
+   *     The pin description for this item. Appears in the content area of the
+   *     pin.
+   *
+   * @return {String}
+   *     Markup for a pin item. Markup should be appened to a list
+   *     (<ul> or <ol>) container as the top-level element in the returned
+   *     markup is a list item.
+   */
   _this.createPinItem = function (className, title, content) {
     return [
       /* eslint-disable indent */
@@ -114,6 +135,10 @@ var AddEditPage = function (options) {
     _this = null;
   }, _this.destroy);
 
+  /**
+   * Fake event handler executed when the example widget action is selected.
+   * Should be removed before production.
+   */
   _this.onAddWidgetClick = function (/*evt*/) {
     ModalView('<p class="alert info">You&rsquo;re Awesome!</p>', {
       title: 'Add Widget Product Interface',
