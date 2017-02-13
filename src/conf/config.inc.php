841d971dd07da46dbf72a5499cd32403d64b8bc5
emartinez-usgs <emartinez@usgs.gov>
2017-02-13 07:50:25 -0700

    Removed stale configuration stuff.

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -9,20 +9,3 @@ if (!file_exists($CONFIG_INI_FILE)) {
 }
 
 $CONFIG = parse_ini_file($CONFIG_INI_FILE);
-
-
-// build absolute Event Page URL string
-$server_protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'Off') ?
-    'https://' : 'http://';
-$server_host = isset($_SERVER['HTTP_HOST']) ?
-    $_SERVER['HTTP_HOST'] : "localhost";
-$server_port = isset($_SERVER['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : 80;
-$HOST_URL_PREFIX = $server_protocol . $server_host;
-if ( ($server_port == 80 && $server_protocol == 'http://') || ($server_port == 443 && $server_protocol == 'https://') ) {
-  // don't need port
-} else {
-  // if a port is specified in the HTTP_HOST, don't use twice (ex: localhost:8080, perhaps used in port forwarding)
-  if(!strpos($server_host, ':')) {
-    $HOST_URL_PREFIX .= ':' . $server_port;
-  }
-}
